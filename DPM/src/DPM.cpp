3dfe967e19804c062649f430335f67da6e8b65de
mehri-bmp <mehri.bmp@gmail.com>
2023-09-21 21:12:31 -0500

    clean up

--- DPM/src/DPM.cpp
+++
@@ -1,3 +0,0 @@
-
-#include "DPMmutau.h"
-#include "DPMNoGa.h"
--- ivbart/R/amode.R
+++
@@ -1,5 +0,0 @@
-amode = function(n,Imode) {### alpha value which gives p(k|..) with mode at Imode
-   egamm = 0.5772156649
-   temp = digamma(Imode) - log(egamm+log(n))
-   return(exp(temp))
-}
